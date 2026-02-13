c443b018c5ee506a2196e847992b359078e138f0
nianod <arnoldkk422@gmail.com>
2026-02-13 15:28:18 +0300

    production code

--- index.js
+++ index.js
@@ -1,95 +1,121 @@
-import dotenv from 'dotenv';
-dotenv.config() 
-import express from 'express'
-import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+// dotenv.config() 
+// import express from 'express'
+// import mongoose from 'mongoose';
  
-import productRoute from './Routes/productRoute.js'
-import Product from './Models/Product.models.js'
-import cors from 'cors'
-import authroutes from './Routes/authroutes.js'
-import userRoutes from './Routes/userRoutes.js'
+// import productRoute from './Routes/productRoute.js'
+// import Product from './Models/Product.models.js'
+// import cors from 'cors'
+// import authroutes from './Routes/authroutes.js'
+// import userRoutes from './Routes/userRoutes.js'
 
-    const app = express()
-    app.use(cors({
-        origin: 'https://arnold-sellers-blond.vercel.app',
-        credentials: true
-    }))
+//     const app = express()
+//     app.use(cors({
+//         origin: 'https://arnold-sellers-blond.vercel.app',
+//         credentials: true
+//     }))
 
 
 
-app.use(express.json()) //Middleware
-app.use(express.urlencoded({extended: false }))
+// app.use(express.json()) //Middleware
+// app.use(express.urlencoded({extended: false }))
 
-app.use('/api/auth', authroutes)
+// app.use('/api/auth', authroutes)
+// app.use('/api/products', productRoute)
+// app.use('/api/user', userRoutes)
+
+// const PORT = process.env.PORT || 7000
+// const MONGO = process.env.MONGO_URL
+
+// app.post('/api/products', async (request, response) => {
+//     try {
+//         const product = await Product.create(request.body)
+//         response.status(201).json(product)
+//     } catch(error) {
+//         response.status(500).json({message: error.message})
+//     }
+// })
+
+
+// app.get('/api/product/:id', async(request, response) => {
+//     try {
+//         const { id } = request.params
+//         const product = await Product.findById(id)
+//         response.status(200).json(product)
+//     } catch (error) {
+//         response.status(500).json({ message: error.message })
+//     }
+// })
+
+
+// app.put('/api/product/:id', async(request, response) => {
+//     try {
+//         const { id } = request.params
+
+//         const product = await Product.findByIdAndUpdate(id, request.body, { new: true })
+
+//         if(!product) {
+//             return response.status(404).json({ message: "Product not found"})
+//         }
+//     } catch (error) {
+        
+//         response.status(201).json(updateProduct)
+//     }
+// })
+
+//  app.delete('/api/product/:id', async(request, response) => {
+//     try {
+//         const { id } = request.params
+
+//         const product = await Product.findByIdAndDelete(id)
+
+//         if(!product) {
+//             return response.status(404).json({ message: "Product not found" })
+//         }
+//         response.status(200).json({ message: "Product deleted successfully"})
+        
+//     } catch (error) {
+//         response.status(500).json({message: error.message})
+//     }
+//  })
+
+
+// mongoose.connect(MONGO, {
+//     dbName: "ecommerce-db",
+// })
+// .then(() => {
+//     app.listen(PORT, () => {
+//         console.log(`App running on port ${PORT}`)
+//     })
+//     app.get('/', (request, response) => {
+//         response.send('Hello From NODE API')
+//     })
+// }).catch((error) => {
+//     console.log('Database connection failed', error.message)
+// })
+
+
+const app = express()
+
+ 
+app.use(cors({ origin: 'https://arnold-sellers-blond.vercel.app', credentials: true }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+ 
+app.get('/', (req, res) => {
+  res.send('Hello From NODE API')
+})
+ 
+app.use('/api/auth', authroutes)    
 app.use('/api/products', productRoute)
 app.use('/api/user', userRoutes)
 
-const PORT = process.env.PORT || 7000
-const MONGO = process.env.MONGO_URL
-
-app.post('/api/products', async (request, response) => {
-    try {
-        const product = await Product.create(request.body)
-        response.status(201).json(product)
-    } catch(error) {
-        response.status(500).json({message: error.message})
-    }
-})
-
-
-app.get('/api/product/:id', async(request, response) => {
-    try {
-        const { id } = request.params
-        const product = await Product.findById(id)
-        response.status(200).json(product)
-    } catch (error) {
-        response.status(500).json({ message: error.message })
-    }
-})
-
-
-app.put('/api/product/:id', async(request, response) => {
-    try {
-        const { id } = request.params
-
-        const product = await Product.findByIdAndUpdate(id, request.body, { new: true })
-
-        if(!product) {
-            return response.status(404).json({ message: "Product not found"})
-        }
-    } catch (error) {
-        
-        response.status(201).json(updateProduct)
-    }
-})
-
- app.delete('/api/product/:id', async(request, response) => {
-    try {
-        const { id } = request.params
-
-        const product = await Product.findByIdAndDelete(id)
-
-        if(!product) {
-            return response.status(404).json({ message: "Product not found" })
-        }
-        response.status(200).json({ message: "Product deleted successfully"})
-        
-    } catch (error) {
-        response.status(500).json({message: error.message})
-    }
- })
-
+ 
 
 mongoose.connect(MONGO, {
-    dbName: "ecommerce-db",
-})
-.then(() => {
-    app.listen(PORT, () => {
-        console.log(`App running on port ${PORT}`)
-    })
-    app.get('/', (request, response) => {
-        response.send('Hello From NODE API')
-    })
-}).catch((error) => {
-    console.log('Database connection failed', error.message)
+  dbName: "ecommerce-db",
+}).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`)
+  })
 })
